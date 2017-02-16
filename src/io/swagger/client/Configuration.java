8364ed7dda6883d23effc5d315c79248ddcc9641
victor <victor.tuxie@gmail.com>
2017-02-15 22:20:38 -0600

    Se generan objetos del API https://docs.botframework.com/en-us/restapi/directline3/#navtitle

---
+++ src/io/swagger/client/Configuration.java
@@ -0,0 +1,22 @@
+package io.swagger.client;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2017-02-15T21:57:26.280-06:00")
+public class Configuration {
+  private static ApiClient defaultApiClient = new ApiClient();
+
+  /**
+   * Get the default API client, which would be used when creating API
+   * instances without providing an API client.
+   */
+  public static ApiClient getDefaultApiClient() {
+    return defaultApiClient;
+   }
+
+  /**
+   * Set the default API client, which would be used when creating API
+   * instances without providing an API client.
+   */
+  public static void setDefaultApiClient(ApiClient apiClient) {
+    defaultApiClient = apiClient;
+  }
+}
